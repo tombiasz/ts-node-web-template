f719a461f7fbbe3e017efb60c3042dfa2037ad90
tombiasz <tomasz.m.nowacki@gmail.com>
2020-01-18 18:28:26 +0100

    use dotenv to load config from .env file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Express, Request, Response } from 'express';
 
+const appPort = process.env.APP_PORT;
+const appName = process.env.APP_NAME;
+
 const app: Express = express();
 
 app.get('/', (req: Request, res: Response) => res.send('hello, world'));
 
-app.listen(8000, () => console.log('server running on 8000'));
+app.listen(appPort, () => console.log(`${appName} server running on ${appPort}`));
