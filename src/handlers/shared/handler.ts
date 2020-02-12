80b5ce0bab0e7cf6dab5eeaa9f1766d4b847c4b0
tombiasz <tomasz.m.nowacki@gmail.com>
2020-02-12 19:09:56 +0100

    fix formatting

--- src/handlers/shared/handler.ts
+++ src/handlers/shared/handler.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 
 export abstract class Handler<TContext extends object = {}> {
-	constructor(
-		protected context: TContext = {} as TContext,
-	) {}
+  constructor(protected context: TContext = {} as TContext) {}
 
-  abstract async handle(req: Request, res: Response): Promise<Response | void>
+  abstract async handle(req: Request, res: Response): Promise<Response | void>;
 }
 
-export function createHandler<TContext extends object, THandler extends Handler<TContext>>(
-	cls: new (context?: TContext) => THandler,
+export function createHandler<
+  TContext extends object,
+  THandler extends Handler<TContext>
+>(
+  cls: new (context?: TContext) => THandler
 ): (
   context?: TContext
 ) => (
@@ -17,17 +18,22 @@ export function createHandler<TContext extends object, THandler extends Handler<
   res: Response,
   next: NextFunction
 ) => Promise<Response | void> {
-	return (context?: TContext) => {
+  return (context?: TContext) => {
     const handler = new cls(context);
 
     return async (req: Request, res: Response, next: NextFunction) => {
       try {
         const result = await handler.handle(req, res);
+
         return result;
-      } catch(error) {
-        req.logger.error(`Error handler in ${cls.name}`, { handler: cls.name, error });
+      } catch (error) {
+        req.logger.error(`Error in handler ${cls.name}`, {
+          handler: cls.name,
+          error
+        });
+
         return next(error);
       }
-    }
-  }
+    };
+  };
 }
