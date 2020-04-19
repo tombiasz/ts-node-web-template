28180b314d01149e58607420be9a6ab762af1039
tombiasz <tomasz.m.nowacki@gmail.com>
2020-04-19 18:55:56 +0200

    remove hello handler

--- src/rest/api/routes.ts
+++ src/rest/api/routes.ts
@@ -1,12 +1,6 @@
 import { Router } from 'express';
-import { createHelloHandler } from './hello';
 import { createUserRoutes } from './users/routes';
 
 export function createApiRoutes(): Router | Router[] {
-  return [
-    Router()
-      .get('/', createHelloHandler({ name: 'Fizz Buzz' }))
-      .post('/', createHelloHandler({ name: 'Foo Bar' })),
-    Router().use('/users', createUserRoutes()),
-  ];
+  return [Router().use('/users', createUserRoutes())];
 }
