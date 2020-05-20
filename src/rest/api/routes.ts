8a698de4230dd9a19ec4bad5295947b014462a7a
tombiasz <tomasz.m.nowacki@gmail.com>
2020-05-20 21:42:05 +0200

    move routes to a single file

--- src/rest/api/routes.ts
+++ src/rest/api/routes.ts
@@ -1,6 +1,45 @@
 import { Router } from 'express';
-import { createUserRoutes } from './users/routes';
+import { asHandler } from '../shared/handler';
+import {
+  createCreateUserValidator,
+  createGetUserValidator,
+  createActivateUserValidator,
+  createAuthenticateValidator,
+} from './users/validators';
+import {
+  getUserHandlerFactory,
+  createUserHandlerFactory,
+  registerUserHandlerFactory,
+  activateUserHandlerFactory,
+  authenticateHandlerFactory,
+} from './users/factories';
 
 export function createApiRoutes(): Router | Router[] {
-  return [Router().use('/users', createUserRoutes())];
+  return [
+    Router().use(
+      '/users',
+      Router()
+        .get('/:id', createGetUserValidator(), asHandler(getUserHandlerFactory))
+        .post(
+          '/',
+          createCreateUserValidator(),
+          asHandler(createUserHandlerFactory),
+        )
+        .post(
+          '/register',
+          createCreateUserValidator(),
+          asHandler(registerUserHandlerFactory),
+        )
+        .post(
+          '/activate/:token',
+          createActivateUserValidator(),
+          asHandler(activateUserHandlerFactory),
+        ),
+    ),
+    Router().post(
+      '/auth',
+      createAuthenticateValidator(),
+      asHandler(authenticateHandlerFactory),
+    ),
+  ];
 }
