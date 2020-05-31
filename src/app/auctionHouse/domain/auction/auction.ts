b1f0b6bd3e55f6c884c464b33241a52767831aed
tombiasz <tomasz.m.nowacki@gmail.com>
2020-05-31 20:26:59 +0200

    add create method to Auction entity

--- src/app/auctionHouse/domain/auction/auction.ts
+++ src/app/auctionHouse/domain/auction/auction.ts
@@ -1,10 +1,14 @@
 import { Entity } from '@app/core';
 import { AuctionId } from './auctionId';
 import { SellerId } from '../seller/sellerId';
+import { ITimeProvider } from '@app/userAccess/core';
 
 interface AuctionProps {
   id: AuctionId;
   sellerId: SellerId;
+  title: string;
+  description: string;
+  createdAt: Date;
 }
 
 export class Auction extends Entity<AuctionProps> {
@@ -14,5 +18,28 @@ export class Auction extends Entity<AuctionProps> {
 
   get sellerId() {
     return this.props.sellerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  static create(
+    props: Omit<AuctionProps, 'id'>,
+    timeProvider: ITimeProvider,
+  ): Auction {
+    return new Auction({
+      ...props,
+      id: AuctionId.generate(),
+      createdAt: timeProvider.getCurrentTime(),
+    });
   }
 }
