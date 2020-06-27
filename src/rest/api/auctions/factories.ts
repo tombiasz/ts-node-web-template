7e4db6176d3ea14b3b227efa3ea44fc2f891f3f6
tombiasz <tomasz.m.nowacki@gmail.com>
2020-06-27 19:47:39 +0200

    introduce withdrawAuctionHandlerFactory

--- src/rest/api/auctions/factories.ts
+++ src/rest/api/auctions/factories.ts
@@ -1,12 +1,18 @@
 import { TimeProvider } from '@utils/timeProvider';
 import { HandlerFactory } from '../../shared/handler';
 import { createAuctionsRepository } from '@database/auction';
-import { RegisterAuction, RegisterAuctionData } from '@app/auctions/useCases';
+import {
+  RegisterAuction,
+  RegisterAuctionData,
+  WithdrawAuction,
+  WithdrawAuctionData,
+} from '@app/auctions/useCases';
 import { RegisterAuctionHandler } from './registerAuctionHandler';
 import { UseCaseWithTransaction } from '@database/core/useCaseWithTransaction';
 import { db } from '@database/core';
 import { Auction } from '@app/auctions/domain/auction';
 import { SellerContext } from './sellerContext';
+import { WithdrawAuctionHandler } from './withdrawAuctionHandler';
 
 export const registerAuctionHandlerFactory: HandlerFactory<RegisterAuctionHandler> = (
   req,
@@ -25,6 +31,28 @@ export const registerAuctionHandlerFactory: HandlerFactory<RegisterAuctionHandle
   });
 
   return new RegisterAuctionHandler({
+    useCase,
+    logger: logger.withContext({ handler: RegisterAuctionHandler.name }),
+  });
+};
+
+export const withdrawAuctionHandlerFactory: HandlerFactory<WithdrawAuctionHandler> = (
+  req,
+) => {
+  const logger = req.logger;
+  const authUser = req.getAuthorizedUser();
+
+  const useCase = new UseCaseWithTransaction<WithdrawAuctionData, void>({
+    db,
+    useCase: new WithdrawAuction({
+      sellerContext: new SellerContext(authUser),
+      auctionRepo: createAuctionsRepository({ logger }),
+      logger: logger.withContext({ useCase: WithdrawAuction.name }),
+      timeProvider: new TimeProvider(),
+    }),
+  });
+
+  return new WithdrawAuctionHandler({
     useCase,
     logger: logger.withContext({ handler: RegisterAuctionHandler.name }),
   });
